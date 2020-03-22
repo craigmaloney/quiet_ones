31bfdb5c066f168ac5714e9fa75db1648b071696
Craig Maloney <craig@decafbad.net>
2020-03-22 16:11:41 -0400

    Splitting out into separate columns, adding inactive flag

--- main.py
+++ main.py
@@ -21,10 +21,12 @@ def main():
         if not(user['deleted'] and user['is_bot']):
             email = user['profile'].get('email')
             if email is not None:
-                users[user['id']] = "{name} ({email})".format(
-                        name=user['profile'].get('real_name'),
-                        email=email)
-                posts_by_user[users[user['id']]] = 0
+                updated = datetime.now() - datetime.fromtimestamp(user['updated'])
+                users[user['id']] = {
+                        'name': user['profile'].get('real_name'),
+                        'email': email,
+                        'inactive': updated.days > 30}
+                posts_by_user[user['id']] = 0
 
     for channel in channels_list['channels']:
         if channel['is_archived'] is True:
@@ -34,16 +36,21 @@ def main():
         for history in client.conversations_history(channel=channel['id'], limit=200):
             for message in history['messages']:
                 if 'user' in message and message['user'] in users:
-                    posts_by_user[users[message['user']]] += 1
+                    posts_by_user[message['user']] += 1
 
     posts_by_user_less_than_threshold = \
         Counter({k: count for k, count in posts_by_user.items() if count <= THRESHOLD})
     with open('quiet_ones.csv', 'w') as csvfile:
-        field_names = ['username', 'count']
+        field_names = ['username', 'email', 'count', 'inactive']
         writer = csv.DictWriter(csvfile, fieldnames=field_names, dialect='excel')
         writer.writeheader()
         for user, count in posts_by_user_less_than_threshold.items():
-            writer.writerow({'username': user, 'count': count})
+            writer.writerow({
+                'username': users[user]['name'],
+                'email': users[user]['email'],
+                'count': count,
+                'inactive': users[user]['inactive']
+                })
 
 
 if __name__ == '__main__':
